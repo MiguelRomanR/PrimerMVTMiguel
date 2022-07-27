65a8d23768dd1f646bb6f22dd957b265ab9851ec
MiguelRomanR <rromanmiguelang@gmail.com>
2022-07-26 23:41:47 -0500

    Agregando Style a la pagina de Bienvenida

--- MVTMiguel/views.py
+++ MVTMiguel/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def welcome(request):
-    return HttpResponse('Hola Bienvenido a mi primer MVT con Django')
+    return render(request, 'welcome.html', context={})
